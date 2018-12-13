2a5e3f05a9a2d0e40048b6ffe3c37d427f8b42ff
robertome <roberto.martin.escribano@alumnos.upm.es>
2018-12-13 23:58:01 +0100

    Incluida generacion de Request y Response para manejo de peticiones HTTP

--- src/FrontController.php
+++ src/FrontController.php
@@ -1,3 +1,43 @@
-<?php declare(strict_types = 1);
+<?php declare(strict_types=1);
 
-echo 'Hello World!';
+namespace MiW\Results;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+$environment = 'development';
+
+/*
+ * Register the error handler
+ */
+$whoops = new \Whoops\Run;
+if ($environment !== 'production') {
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+} else {
+    $whoops->pushHandler(function ($e) {
+        echo 'Todo: Friendly error page and send an email to the developer';
+    });
+}
+$whoops->register();
+
+/*
+ * sets up the Request and Response objects that you can use in your other classes
+ */
+$request = new \Http\HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
+$response = new \Http\HttpResponse;
+
+
+
+
+
+
+
+
+/*
+ * send the response data to the browser
+ */
+foreach ($response->getHeaders() as $header) {
+    header($header, false);
+}
+echo $response->getContent();
